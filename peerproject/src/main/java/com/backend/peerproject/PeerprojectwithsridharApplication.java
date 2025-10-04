71740cef37f8f621589cfa1d47a2b0298901500a
hemanth <phemanth4647@gmail.com>
2025-10-04 07:01:58 +0000

    add basic structure

---
+++ peerproject/src/main/java/com/backend/peerproject/PeerprojectwithsridharApplication.java
@@ -0,0 +1,13 @@
+package com.backend.peerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerprojectwithsridharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerprojectwithsridharApplication.class, args);
+	}
+
+}
---
+++ peerproject/src/test/java/com/backend/peerproject/PeerprojectwithsridharApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.peerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerprojectwithsridharApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
