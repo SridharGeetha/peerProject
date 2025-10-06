d3600b66e450ce067aab0b5f00688b8232909199
hemanth <phemanth4647@gmail.com>
2025-10-06 08:20:38 +0000

    peerproject reshaped

--- peerproject/src/main/java/com/backend/peerproject/PeerprojectwithsridharApplication.java
+++ peerproject/src/main/java/com/backend/peerproject/PeerprojectwithsridharApplication.java
@@ -3,10 +3,19 @@ package com.backend.peerproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class PeerprojectwithsridharApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		// Set system properties so Spring Boot can read them
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
 		SpringApplication.run(PeerprojectwithsridharApplication.class, args);
 	}
 
